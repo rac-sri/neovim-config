aed3a677c328894582a9a668500f0e8576b89d72
rac-sri <rac.sri25@gmail.com>
2025-12-26 23:54:42 +0530

    feat: offgrid

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -1,24 +1,25 @@
 return {
-  'goolord/alpha-nvim',
-  dependencies = {
-    'nvim-tree/nvim-web-devicons',
-  },
+	"goolord/alpha-nvim",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
 
-  config = function()
-    local alpha = require 'alpha'
-    local dashboard = require 'alpha.themes.startify'
+	config = function()
+		local alpha = require("alpha")
+		local dashboard = require("alpha.themes.startify")
 
-    dashboard.section.header.val = {
-      [[                                                    ]],
-      [[ ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ]],
-      [[ ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ]],
-      [[ ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ]],
-      [[ ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ]],
-      [[ ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ]],
-      [[ ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ]],
-      [[                                                    ]],
-    }
+		dashboard.section.header.val = {
+			[[                                                    ]],
+			[[  ██████╗ ███████╗███████╗ ██████╗ ██████╗ ██╗██████╗ ]],
+			[[ ██╔═══██╗██╔════╝██╔════╝██╔════╝ ██╔══██╗██║██╔══██╗]],
+			[[ ██║   ██║█████╗  █████╗  ██║  ███╗██████╔╝██║██║  ██║]],
+			[[ ██║   ██║██╔══╝  ██╔══╝  ██║   ██║██╔══██╗██║██║  ██║]],
+			[[ ╚██████╔╝██║     ██║     ╚██████╔╝██║  ██║██║██████╔╝]],
+			[[  ╚═════╝ ╚═╝     ╚═╝      ╚═════╝ ╚═╝  ╚═╝╚═╝╚═════╝ ]],
+			[[                                                    ]],
+			[[                     only local state                ]],
+		}
 
-    alpha.setup(dashboard.opts)
-  end,
+		alpha.setup(dashboard.opts)
+	end,
 }
